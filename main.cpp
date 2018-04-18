bce8639e24a5d7d2908aa11fee2ec55806b9efed
pengjiawei <917527280@qq.com>
2018-04-18 13:01:48 +0800

    fixed

--- main.cpp
+++ main.cpp
@@ -2,38 +2,42 @@
 #define ERROR -1;
 typedef char elem_type;
 struct binary_node{
-    elem_type data;
+    char data;
     binary_node* left;
     binary_node* right;
 };
 //default preorder create
-int create_tree(binary_node* b){
+int create_tree(binary_node** b){
     char c;
-    setbuf(stdin,NULL);
     scanf("%c",&c);
     if (c == '#'){
-        b = NULL;
+        *b = NULL;
     }else{
-        b = (binary_node *) malloc(sizeof(binary_node));
+        //这里注意，如果传入进来的是*b 那么走到这里重新分配内存空间之后就不是在操作实参了。
+        *b =  (binary_node*)malloc(sizeof(binary_node));
         if (b == NULL)
             return ERROR;
-        b->data = c;
-        create_tree(b->left);
-        create_tree(b->right);
+        (*b)->data = c;
+        create_tree(&(*b)->left);
+        create_tree(&(*b)->right);
     }
 };
 //preorder traversal
 void preorder(binary_node* b){
-    if(b){
-        printf("data = %c,",b->data);
+    if(b != NULL){
+        printf("%c,",b->data);
         preorder(b->left);
         preorder(b->right);
     }
 }
 int main() {
+    //传递指针的引用
+    setbuf(stdin,NULL);
     binary_node* b = NULL;
-    create_tree(b);
+    create_tree(&b);
+    printf("pre order\n");
     preorder(b);
+    printf("\n");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
